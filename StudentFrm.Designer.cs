33904f415612412c4471df37e51828a52e42d1c8
Pacifique16 <harerimanpacifique95@gmail.com>
2026-02-11 18:43:58 +0200

    initial commit student management system

---
+++ StudentFrm.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class StudentFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SaveBtn = new System.Windows.Forms.Button();
+            this.studentIDBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.fullNameBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.addressBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.UpdateBrn = new System.Windows.Forms.Button();
+            this.DeleteBtn = new System.Windows.Forms.Button();
+            this.dobPicker = new System.Windows.Forms.DateTimePicker();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // SaveBtn
+            // 
+            this.SaveBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(204)))), ((int)(((byte)(113)))));
+            this.SaveBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.SaveBtn.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+            this.SaveBtn.ForeColor = System.Drawing.Color.White;
+            this.SaveBtn.Location = new System.Drawing.Point(850, 550);
+            this.SaveBtn.Name = "SaveBtn";
+            this.SaveBtn.Size = new System.Drawing.Size(120, 40);
+            this.SaveBtn.TabIndex = 13;
+            this.SaveBtn.Text = "Save";
+            this.SaveBtn.UseVisualStyleBackColor = false;
+            this.SaveBtn.Click += new System.EventHandler(this.SaveBtn_Click);
+            // 
+            // studentIDBox
+            // 
+            this.studentIDBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.studentIDBox.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.studentIDBox.Location = new System.Drawing.Point(850, 120);
+            this.studentIDBox.Name = "studentIDBox";
+            this.studentIDBox.Size = new System.Drawing.Size(350, 27);
+            this.studentIDBox.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label2.Location = new System.Drawing.Point(850, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Student ID";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
+            this.label1.Location = new System.Drawing.Point(850, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(318, 37);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Student Management";
+            // 
+            // fullNameBox
+            // 
+            this.fullNameBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.fullNameBox.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.fullNameBox.Location = new System.Drawing.Point(850, 200);
+            this.fullNameBox.Name = "fullNameBox";
+            this.fullNameBox.Size = new System.Drawing.Size(350, 27);
+            this.fullNameBox.TabIndex = 15;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label3.Location = new System.Drawing.Point(850, 175);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 20);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Full Name";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label5.Location = new System.Drawing.Point(850, 335);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(96, 20);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Date of Birth";
+            // 
+            // addressBox
+            // 
+            this.addressBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.addressBox.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.addressBox.Location = new System.Drawing.Point(850, 280);
+            this.addressBox.Name = "addressBox";
+            this.addressBox.Size = new System.Drawing.Size(350, 27);
+            this.addressBox.TabIndex = 17;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label7.Location = new System.Drawing.Point(850, 255);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(62, 20);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Address";
+            // 
+            // UpdateBrn
+            // 
+            this.UpdateBrn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(152)))), ((int)(((byte)(219)))));
+            this.UpdateBrn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.UpdateBrn.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+            this.UpdateBrn.ForeColor = System.Drawing.Color.White;
+            this.UpdateBrn.Location = new System.Drawing.Point(990, 550);
+            this.UpdateBrn.Name = "UpdateBrn";
+            this.UpdateBrn.Size = new System.Drawing.Size(120, 40);
+            this.UpdateBrn.TabIndex = 20;
+            this.UpdateBrn.Text = "Update";
+            this.UpdateBrn.UseVisualStyleBackColor = false;
+            this.UpdateBrn.Click += new System.EventHandler(this.UpdateBrn_Click);
+            // 
+            // DeleteBtn
+            // 
+            this.DeleteBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(76)))), ((int)(((byte)(60)))));
+            this.DeleteBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.DeleteBtn.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+            this.DeleteBtn.ForeColor = System.Drawing.Color.White;
+            this.DeleteBtn.Location = new System.Drawing.Point(850, 610);
+            this.DeleteBtn.Name = "DeleteBtn";
+            this.DeleteBtn.Size = new System.Drawing.Size(260, 40);
+            this.DeleteBtn.TabIndex = 21;
+            this.DeleteBtn.Text = "Delete";
+            this.DeleteBtn.UseVisualStyleBackColor = false;
+            this.DeleteBtn.Click += new System.EventHandler(this.DeleteBtn_Click);
+            // 
+            // dobPicker
+            // 
+            this.dobPicker.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.dobPicker.Location = new System.Drawing.Point(850, 360);
+            this.dobPicker.Name = "dobPicker";
+            this.dobPicker.Size = new System.Drawing.Size(350, 27);
+            this.dobPicker.TabIndex = 22;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(30, 30);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(780, 620);
+            this.dataGridView1.TabIndex = 23;
+            // 
+            // StudentFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(1250, 700);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dobPicker);
+            this.Controls.Add(this.DeleteBtn);
+            this.Controls.Add(this.UpdateBrn);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.addressBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.fullNameBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.SaveBtn);
+            this.Controls.Add(this.studentIDBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "StudentFrm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Student Management System";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SaveBtn;
+        private System.Windows.Forms.TextBox studentIDBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox fullNameBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox addressBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button UpdateBrn;
+        private System.Windows.Forms.Button DeleteBtn;
+        private System.Windows.Forms.DateTimePicker dobPicker;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
---
+++ StudentFrm.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace WindowsFormsApp1
+{
+    public partial class StudentFrm : Form
+    {
+        public StudentFrm()
+        {
+            InitializeComponent();
+            LoadStudents();
+            dataGridView1.CellClick += DataGridView1_CellClick;
+        }
+
+        private void LoadStudents()
+        {
+            string con = ConfigurationManager.ConnectionStrings["conString"].ConnectionString;
+            using (SqlConnection conConn = new SqlConnection(con))
+            {
+                conConn.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM STUDENT", conConn);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+        }
+
+        private void DataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+                studentIDBox.Text = row.Cells["id"].Value.ToString();
+                fullNameBox.Text = row.Cells["name"].Value.ToString();
+                addressBox.Text = row.Cells["address"].Value.ToString();
+                dobPicker.Value = Convert.ToDateTime(row.Cells["dob"].Value);
+            }
+        }
+        private void SaveBtn_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(studentIDBox.Text) || string.IsNullOrWhiteSpace(fullNameBox.Text))
+            {
+                MessageBox.Show("Please fill Student ID and Full Name");
+                return;
+            }
+
+            string con = ConfigurationManager.ConnectionStrings["conString"].ConnectionString;
+            using (SqlConnection conConn = new SqlConnection(con))
+            {
+                try
+                {
+                    conConn.Open();
+                    SqlCommand cmd = new SqlCommand("INSERT INTO STUDENT VALUES(@id,@name,@add,@dob)", conConn);
+                    cmd.Parameters.AddWithValue("@id", studentIDBox.Text.Trim());
+                    cmd.Parameters.AddWithValue("@name", fullNameBox.Text.Trim());
+                    cmd.Parameters.AddWithValue("@add", addressBox.Text.Trim());
+                    cmd.Parameters.AddWithValue("@dob", dobPicker.Value);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Student created with Success");
+                    LoadStudents();
+                    ClearFields();
+                }
+                catch (SqlException ex)
+                {
+                    if (ex.Number == 2627)
+                        MessageBox.Show("Student ID already exists");
+                    else
+                        MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+        }
+
+        private void UpdateBrn_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(studentIDBox.Text) || string.IsNullOrWhiteSpace(fullNameBox.Text))
+            {
+                MessageBox.Show("Please fill Student ID and Full Name");
+                return;
+            }
+
+            string con = ConfigurationManager.ConnectionStrings["conString"].ConnectionString;
+            using (SqlConnection conConn = new SqlConnection(con))
+            {
+                try
+                {
+                    conConn.Open();
+                    SqlCommand cmd = new SqlCommand("UPDATE STUDENT SET name=@name, address=@add, dob=@dob WHERE id=@id", conConn);
+                    cmd.Parameters.AddWithValue("@id", studentIDBox.Text.Trim());
+                    cmd.Parameters.AddWithValue("@name", fullNameBox.Text.Trim());
+                    cmd.Parameters.AddWithValue("@add", addressBox.Text.Trim());
+                    cmd.Parameters.AddWithValue("@dob", dobPicker.Value);
+                    int rows = cmd.ExecuteNonQuery();
+                    if (rows > 0)
+                    {
+                        MessageBox.Show("Student updated successfully");
+                        LoadStudents();
+                        ClearFields();
+                    }
+                    else
+                        MessageBox.Show("Student ID not found");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+        }
+
+        private void DeleteBtn_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(studentIDBox.Text))
+            {
+                MessageBox.Show("Please select a student to delete");
+                return;
+            }
+
+            var result = MessageBox.Show("Are you sure you want to delete this student?", "Confirm Delete", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (result == DialogResult.No)
+                return;
+
+            string con = ConfigurationManager.ConnectionStrings["conString"].ConnectionString;
+            using (SqlConnection conConn = new SqlConnection(con))
+            {
+                try
+                {
+                    conConn.Open();
+                    SqlCommand cmd = new SqlCommand("DELETE FROM STUDENT WHERE id=@id", conConn);
+                    cmd.Parameters.AddWithValue("@id", studentIDBox.Text.Trim());
+                    int rows = cmd.ExecuteNonQuery();
+                    if (rows > 0)
+                    {
+                        MessageBox.Show("Student deleted successfully");
+                        LoadStudents();
+                        ClearFields();
+                    }
+                    else
+                        MessageBox.Show("Student ID not found");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+        }
+
+        private void ClearFields()
+        {
+            studentIDBox.Clear();
+            fullNameBox.Clear();
+            addressBox.Clear();
+            dobPicker.Value = DateTime.Now;
+        }
+    }
+}
